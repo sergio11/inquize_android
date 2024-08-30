1edeefa82257d720790bedef605eace3db992b8d
Sergio Sánchez <sss4esob@gmail.com>
2024-08-30 20:07:49 +0200

    Implement several improvements in InquizeRepositoryImpl

--- app/src/main/java/com/dreamsoftware/inquize/data/repository/impl/InquizeRepositoryImpl.kt
+++ app/src/main/java/com/dreamsoftware/inquize/data/repository/impl/InquizeRepositoryImpl.kt
@@ -5,8 +5,14 @@ import com.dreamsoftware.inquize.data.remote.datasource.IImageDataSource
 import com.dreamsoftware.inquize.data.remote.datasource.IInquizeDataSource
 import com.dreamsoftware.inquize.data.remote.dto.InquizeDTO
 import com.dreamsoftware.inquize.data.remote.dto.SaveInquizeDTO
+import com.dreamsoftware.inquize.data.remote.exception.DeleteInquizeByIdRemoteDataException
+import com.dreamsoftware.inquize.data.remote.exception.FetchAllInquizeRemoteDataException
+import com.dreamsoftware.inquize.data.remote.exception.FetchInquizeByIdRemoteDataException
 import com.dreamsoftware.inquize.data.remote.exception.SaveInquizeRemoteDataException
 import com.dreamsoftware.inquize.data.repository.impl.core.SupportRepositoryImpl
+import com.dreamsoftware.inquize.domain.exception.DeleteInquizeByIdException
+import com.dreamsoftware.inquize.domain.exception.FetchAllInquizeException
+import com.dreamsoftware.inquize.domain.exception.FetchInquizeByIdException
 import com.dreamsoftware.inquize.domain.exception.SaveInquizeException
 import com.dreamsoftware.inquize.domain.model.InquizeBO
 import com.dreamsoftware.inquize.domain.model.SaveInquizeBO
@@ -30,7 +36,44 @@ internal class InquizeRepositoryImpl(
                 inquizeMapper.mapInToOut(fetchById(userId = data.userId, id = data.uid))
             }
         } catch (ex: SaveInquizeRemoteDataException) {
+            ex.printStackTrace()
             throw SaveInquizeException("An error occurred when trying to save inquize", ex)
+        }
+    }
+
+    @Throws(DeleteInquizeByIdException::class)
+    override suspend fun deleteById(userId: String, id: String) {
+        safeExecute {
+            try {
+                imageDataSource.deleteByName(name = id)
+                inquizeDataSource.deleteById(userId = userId, id = id)
+            } catch (ex: DeleteInquizeByIdRemoteDataException) {
+                ex.printStackTrace()
+                throw DeleteInquizeByIdException("An error occurred when trying to delete the inquize", ex)
+            }
+        }
+    }
+
+    @Throws(FetchInquizeByIdException::class)
+    override suspend fun fetchById(userId: String, id: String): InquizeBO = safeExecute {
+        try {
+            inquizeDataSource.fetchById(userId = userId, id = id)
+                .let(inquizeMapper::mapInToOut)
+        } catch (ex: FetchInquizeByIdRemoteDataException) {
+            ex.printStackTrace()
+            throw FetchInquizeByIdException("An error occurred when trying to fetch the inquize data", ex)
+        }
+    }
+
+    @Throws(FetchAllInquizeException::class)
+    override suspend fun fetchAllByUserId(userId: String): List<InquizeBO> = safeExecute {
+        try {
+            inquizeDataSource.fetchAllByUserId(userId = userId)
+                .let(inquizeMapper::mapInListToOutList)
+                .toList()
+        } catch (ex: FetchAllInquizeRemoteDataException) {
+            ex.printStackTrace()
+            throw FetchAllInquizeException("An error occurred when trying to fetch all user questions", ex)
         }
     }
 }
--- app/src/main/java/com/dreamsoftware/inquize/domain/repository/IInquizeRepository.kt
+++ app/src/main/java/com/dreamsoftware/inquize/domain/repository/IInquizeRepository.kt
@@ -1,5 +1,8 @@
 package com.dreamsoftware.inquize.domain.repository
 
+import com.dreamsoftware.inquize.domain.exception.DeleteInquizeByIdException
+import com.dreamsoftware.inquize.domain.exception.FetchAllInquizeException
+import com.dreamsoftware.inquize.domain.exception.FetchInquizeByIdException
 import com.dreamsoftware.inquize.domain.exception.SaveInquizeException
 import com.dreamsoftware.inquize.domain.model.InquizeBO
 import com.dreamsoftware.inquize.domain.model.SaveInquizeBO
@@ -8,4 +11,13 @@ interface IInquizeRepository {
 
     @Throws(SaveInquizeException::class)
     suspend fun save(data: SaveInquizeBO): InquizeBO
+
+    @Throws(DeleteInquizeByIdException::class)
+    suspend fun deleteById(userId: String, id: String)
+
+    @Throws(FetchInquizeByIdException::class)
+    suspend fun fetchById(userId: String, id: String): InquizeBO
+
+    @Throws(FetchAllInquizeException::class)
+    suspend fun fetchAllByUserId(userId: String): List<InquizeBO>
 }
